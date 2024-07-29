0cb913e25134c7d3ff496d4a9bff317104fd69fe
PunThePancake <alt1@logie.lol>
2024-07-29 18:50:30 +1000

    pfp update

--- shop/app.js
+++ shop/app.js
@@ -144,12 +144,24 @@ if (user) {
 }
 
 if (siteToken === null) {
+    //logged out
     logoutButtonNav.style.display = 'none';
     loginWarning.style.display = 'flex';
     shopWrapper.style.display = 'none';
     mainShop.style.display = 'none';
 
 } else {
+    //logged in
+
+    const {id, avatar} = JSON.parse(user).user.discord;
+
+    if (avatar !== null) {
+        const pfp = `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`;
+        document.getElementById('logoutProfilePicture').src = pfp;
+    } else {
+        document.getElementById('logoutProfilePicture').src = "../default-pfp.png";
+    }
+
     loginButtonNav.style.display = 'none';
     loginWarning.style.display = 'none';
     shopWrapper.style.display = 'flex';
